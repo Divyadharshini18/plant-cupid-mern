666fc0ca09dbf0df26d14582ee6a48a3a9832109
Divyadharshini18 <divyasadhu725@gmail.com>
2025-12-18 16:00:53 +0530

    feat: add User model schema

--- backend/server.js
+++ backend/server.js
@@ -19,3 +19,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const User = require("./models/User");
+console.log("User model loaded:", User.modelName);
