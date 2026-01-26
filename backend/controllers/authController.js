d8eba260258fa044adbf17f506a0f96cda05fc56
Divyadharshini18 <divyasadhu725@gmail.com>
2026-01-26 11:55:35 +0530

    eroor msg rectified

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,7 +15,7 @@ const registerUser = async (req, res) => {
     const userExists = await User.findOne({ email });
     if (userExists) {
       console.log("registerUser - User already exists:", email);
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(409).json({ message: "User already exists" });
     }
 
     console.log("registerUser - Creating new user:", { name, email });
