4f0c13f6b37a028e37d313630456046ead1bfb04
Divyadharshini18 <divyasadhu725@gmail.com>
2026-01-30 22:04:03 +0530

    db.js file changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // mongoose - works between JS and MongoDB
 
-const connectDB = async () => {
+const connectDB = async () => { // async functions are functions that return a promise and make the JS wait inside it using await only the function the other events takes place, await only can be used in the async functions
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1); // crash app if DB fails
+    await mongoose.connect(process.env.MONGO_URI); // wait until mongoose connects to the MongoDB using the connection string stored in environment variable MONGO_URI
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1); 
   }
 };
 
-module.exports = connectDB;
+module.exports = connectDB; // exporting the connectDB function to be used in other files
+
+// process refers to the current Node.js process
+// exit(1) is used to exit the process with a failure code and 0 for sucess
+
+// promise are object that will available later in the future that allows JS to continue running and come back when task finshes
+// promise has three states: pending, fulfilled, rejected
