3b16e0bba5426b109a9582d8069ae8a89a223575
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-02 13:27:13 +0530

    authController.js rectified

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,39 +1,27 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-};
+const generateToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 
 const registerUser = async (req, res) => {
-  console.log("registerUser controller - Starting registration");
   const { name, email, password } = req.body;
 
   try {
     const userExists = await User.findOne({ email });
     if (userExists) {
-      console.log("registerUser - User already exists:", email);
       return res.status(409).json({ message: "User already exists" });
     }
 
-    console.log("registerUser - Creating new user:", { name, email });
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
+    const user = await User.create({ name, email, password });
 
-    console.log("registerUser - User created successfully:", user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
       token: generateToken(user._id),
     });
-  } catch (error) {
-    console.error("registerUser - Error:", error);
+  } catch (err) {
     res.status(500).json({ message: "Server error during registration" });
   }
 };
@@ -44,15 +32,15 @@ const loginUser = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    res.json({
+    return res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       token: generateToken(user._id),
     });
-  } else {
-    res.status(401).json({ message: "Invalid credentials" });
   }
+
+  res.status(401).json({ message: "Invalid credentials" });
 };
 
 module.exports = { registerUser, loginUser };
