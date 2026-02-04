6ead107e89ada31b0a16fe04d523bf9d8d1ada9b
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-04 16:25:27 +0530

    User Model

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       minlength: 6,
-    },
+    }, // stored as hashed value
 
     bio: {
       type: String,
@@ -28,24 +28,22 @@ const userSchema = new mongoose.Schema(
     },
   },
   {
-    timestamps: true,
+    timestamps: true, // automatically adds createdAt and updatedAt feilds
   }
 );
 
-// Hash password before saving
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
     return;
-  }
+  } // if password is not modified, skip hashing
 
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(10); // random data added to password before hashing , makes hashing slow by cost factor of 10 , ~2^10 = 1024 rounds
   this.password = await bcrypt.hash(this.password, salt);
 });
 
 
-// Compare password during login
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
-};
+}; // Compare password during login and return true or false
 
 module.exports = mongoose.model("User", userSchema);
