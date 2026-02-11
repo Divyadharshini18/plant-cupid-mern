ebe358f9dc49658f684ec011c5087e9e8e266aab
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-11 13:09:51 +0530

    Com: Auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,9 @@
-const express = require("express");
-const {
-  registerUser,
-  loginUser,
-} = require("../controllers/authController");
+const { Router } = require("express");
+const { registerUser, loginUser } = require("../controllers/authController");
 
-const router = express.Router();
+const router = Router();
 
 router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/login", loginUser); // tells which controller function to run when certain URL is hit
 
 module.exports = router;
