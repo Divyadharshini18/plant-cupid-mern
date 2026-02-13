851538aff08d9c1bcb526b0572a6487c014d82a1
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-13 11:50:33 +0530

    Com: api.jsx

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,20 +1,20 @@
-import axios from "axios";
+import axios from "axios"; // axios promise-based http client for node.js and the browser also isomorphic
 
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
-});
+}); // instance of axios with base URL
 
-api.interceptors.request.use(
+api.interceptors.request.use( // interceptors are middleware for axios : runs before every request is sent
   (config) => {
     console.log(
-      `API Request: ${config.method?.toUpperCase()} ${config.baseURL}${config.url}`
+      `API Request: ${config.method?.toUpperCase()} ${config.baseURL}${config.url}` // ? optional chaining to avoid crash and safer logging
     );
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error) // if something fails before request is sent, reject promise
 );
 
-api.interceptors.response.use(
+api.interceptors.response.use( // after response is received
   (response) => {
     console.log(`API Response: ${response.status} ${response.config.url}`);
     return response;
