f820b0d926e9b549215b865067e4a8ea86cae84a
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-24 10:47:56 +0530

    Add: Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to PlantCupid!</h1>
+            <h2>Plant it. Water it. Grow with it.</h2>
+        </div>
+    )
+}
+
+export default Home;
