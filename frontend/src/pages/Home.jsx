bc7989ee0a716600880769822cf21a3142205361
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-25 12:05:00 +0530

    Home page UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
+import '../index.css';
+
 function Home() {
     return (
         <div className="home-bg">
-            <h1>Welcome to PlantCupid!</h1>
-            <h2>Plant it. Water it. Grow with it.</h2>
+            <h1>Welcome to PlantCupid!</h1><br />
+            <h3>Plant it. Water it. Grow with it.</h3>
             <div className="log-sign-buttons">
-                <button onClick={() => window.location.href="/login"}>Login in</button>
+                <button onClick={() => window.location.href="/login"}>Login</button>
                 <button onClick={() => window.location.href="/signup"}>Sign up</button>
             </div>
         </div>
