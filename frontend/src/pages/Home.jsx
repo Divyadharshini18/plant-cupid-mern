6d84ffd636c4a5f8ecb9a2a76bd96eefb2be0791
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-25 11:03:28 +0530

    Mod: Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,10 @@ function Home() {
         <div className="home-bg">
             <h1>Welcome to PlantCupid!</h1>
             <h2>Plant it. Water it. Grow with it.</h2>
-            <button >Login in</button>
-            <button>Sign up</button>
+            <div className="log-sign-buttons">
+                <button onClick={() => window.location.href="/login"}>Login in</button>
+                <button onClick={() => window.location.href="/signup"}>Sign up</button>
+            </div>
         </div>
     )
 }
