0d26fa78f3318902335a3390662ad660a41b5cac
Divyadharshini18 <divyasadhu725@gmail.com>
2026-02-26 13:41:47 +0530

    Home page : typing text

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import '../index.css';
 
 function Home() {
+
+    const text = "Plant it. Grow with it.";
+    const [display, setDisplay] = useState("");
+    const [isDelete, setIsDelete] = useState(false);
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const speed = isDelete ? 50 : 100;
+        const timer = setTimeout(() => {
+            if(!isDelete) {
+                setDisplay(text.slice(0, index+1));
+                setIndex(index+1);
+                if(index+1 == text.length){
+                    setTimeout(() => setIsDelete(true), 1000);
+                }
+            }
+            else{
+                setDisplay(text.slice(0, index-1));
+                setIndex(index-1);
+                if(index-1 == 0){
+                    setIsDelete(false);
+                }
+            }
+        }, speed);
+        return () => clearTimeout(timer);
+    }, [index, isDelete, text]);
+
     return (
         <div className="home-bg">
             <h1>Welcome to PlantCupid!</h1><br />
-            <h3>Plant it. Water it. Grow with it.</h3>
+            <h3 className='typing-text'>{display}</h3>
             <div className="log-sign-buttons">
                 <button onClick={() => window.location.href="/login"}>Login</button>
                 <button onClick={() => window.location.href="/signup"}>Sign up</button>
