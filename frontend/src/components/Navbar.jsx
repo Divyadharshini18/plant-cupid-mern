be736a83f8e2ce043d9a3b066f009f3fcf570695
Divyadharshini18 <divyasadhu725@gmail.com>
2026-03-09 16:05:39 +0530

    Media queries - Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,27 +4,35 @@ import { useAuth } from "../context/AuthContext";
 import "../index.css";
 
 function Navbar() {
-  const { user, logout } = useAuth();
+  const { logout } = useAuth();
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    logout();            // clear auth state
-    navigate("/login");  // redirect to login page
+    logout();
+    navigate("/login");
   };
 
   return (
-    <div className="navbar">
-      <div className="title">Plant Cupid | </div>
+    <nav className="navbar">
 
-      <Link to="/dashboard" className="nav-link">Dashboard</Link>
-      <Link to="/plants" className="nav-link">Plants</Link>
-      <Link to="/profile" className="nav-link">Profile</Link>
-      <Link to="/about" className="nav-link">About Us</Link>
+      <div className="nav-left">
+        <h2 className="title">Plant Cupid | </h2>
+      </div>
+
+      <div className="nav-links">
+        <Link to="/dashboard" className="nav-link">Dashboard</Link>
+        <Link to="/plants" className="nav-link">Plants</Link>
+        <Link to="/profile" className="nav-link">Profile</Link>
+        <Link to="/about" className="nav-link">About Us</Link>
+      </div>
 
       <div className="nav-right">
-        <button className="nav-logout" onClick={handleLogout}> Logout </button>
+        <button className="nav-logout" onClick={handleLogout}>
+          Logout
+        </button>
       </div>
-    </div>
+
+    </nav>
   );
 }
 
