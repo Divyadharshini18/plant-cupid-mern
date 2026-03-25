5bd76ad8f9ae01dc3fd5fee13809d3e63b65daf7
Divyadharshini18 <divyasadhu725@gmail.com>
2026-03-25 10:12:28 +0530

    protected layout changed

--- frontend/src/components/ProtectedLayout.jsx
+++ frontend/src/components/ProtectedLayout.jsx
@@ -3,9 +3,9 @@ import Navbar from "./Navbar";
 
 export default function ProtectedLayout() {
   return (
-    <>
+    <div className="app-layout">
       <Navbar />
       <Outlet />
-    </>
+    </div>
   );
 }
