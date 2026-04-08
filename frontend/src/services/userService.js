b76bd79fd09c324e74090ba12d8021a2c26837ca
Divyadharshini18 <divyasadhu725@gmail.com>
2026-04-08 22:39:57 +0530

    User service added

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,22 @@
+import api from "../api/api";
+import { cacheKeys, readCache, writeCache, clearProfileCache } from "./cache";
+
+const getAuthHeaders = (token) => ({
+  headers: { Authorization: `Bearer ${token}` },
+});
+
+export const getProfile = async (token, { force = false } = {}) => {
+  if (!force) {
+    const cached = readCache(cacheKeys.profile, null);
+    if (cached) return cached;
+  }
+
+  const res = await api.get("/users/profile", getAuthHeaders(token));
+  writeCache(cacheKeys.profile, res.data);
+  return res.data;
+};
+
+export const refreshProfile = async (token) => {
+  clearProfileCache();
+  return getProfile(token, { force: true });
+};
